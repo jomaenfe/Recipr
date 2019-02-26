43c108b8d2d3db94ff3e663250a66ad2469f2dcb
Andrea Morales <andreamorgarz@gmail.com>
2019-02-26 13:21:42 +0100

    add test

--- italian_database.py
+++ italian_database.py
@@ -2,26 +2,25 @@ import pyexcel as pe
 
 from correspondences import get_correspondences
 
-def process_german_db(fields_correspondences,filename = "bd-alemana.xlsx"):
+def process_italian_db(fields_correspondences,filename = "bd-italiana.xlsx"):
 
     ## Read the sheet (we need to specify the correct sheet of the book)
     sheet = pe.get_sheet(file_name=filename)
 
     # Get field names
-    row_names = sheet.row[2]
-    print(row_names)
+    row_names = sheet.row[1]
     n_fields = len(row_names)
 
     for i in range(n_fields):
         row_names[i] = fields_correspondences[row_names[i]]
 
     # Save all the changes
-    sheet.row[2] = row_names
-    sheet.save_as("processed-german-db.xlsx")
+    sheet.row[1] = row_names
+    sheet.save_as("processed-italian-db.xlsx")
 
     pass
 
 
 if __name__ == "__main__":
     fields_correspondences = get_correspondences()
-    process_german_db(fields_correspondences)
+    process_italian_db(fields_correspondences)
