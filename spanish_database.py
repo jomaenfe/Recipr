ce9c38e11ee7ef930e9e614e871e1313c5ef4aa4
Andrea Morales <andreamorgarz@gmail.com>
2019-02-27 10:50:53 +0100

    #2 add spanish database

---
+++ spanish_database.py
@@ -0,0 +1,30 @@
+import pyexcel as pe
+
+from correspondences import get_correspondences
+
+# ------------------------------------------------------------------------------
+def process_spanish_db(fields_correspondences,filename = "data/espanola.xlsx"):
+
+    ## Read the sheet (we need to specify the correct sheet of the book)
+    sheet = pe.get_sheet(file_name=filename)
+
+    # Get field names
+    row_names = sheet.row[0]
+    # print(row_names)
+    n_fields = len(row_names)
+
+    for i in range(n_fields):
+        row_names[i] = fields_correspondences[row_names[i]]
+
+    # Save all the changes
+    sheet.row[0] = row_names
+    sheet.save_as("data/processed-spanish-db.xlsx")
+
+    pass
+
+
+# ------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    fields_correspondences = get_correspondences()
+    process_greek_db(fields_correspondences)
