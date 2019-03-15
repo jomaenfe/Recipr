db3b42d6e4189f82453de367fb8e12d2b02c2b19
Gecofer <gema.cofer95@gmail.com>
2019-03-15 13:45:42 +0100

    #2 Remove test to greek database

--- test/test_correspondences.py
+++ test/test_correspondences.py
@@ -7,7 +7,7 @@ from correspondences import get_correspondences
 from english_database import process_english_db
 from german_database import process_german_db
 from italian_database import process_italian_db
-from greek_database import process_greek_db
+# from greek_database import process_greek_db
 from spanish_database import process_spanish_db
 
 
@@ -49,20 +49,20 @@ class correspondencesTestCase(unittest.TestCase):
         self.assertEqual(new_row_names[-1], "hexadecatetraenoic-acid-mg", "The content of the fields doesn't match")
         pass
 
-    def test_greek_db(self):
-
-        # old_row_names = greek_example_sheet.row[1]
-
-        process_greek_db(self.fields_correspondences)
-        greek_db_to_test = pe.get_sheet(file_name="data/processed-greek-db.xlsx")
-        new_row_names = greek_db_to_test.row[0]
-
-        # Check first and last name fields
-        self.assertEqual(new_row_names[0], "food-name", "The content of the fields doesn't match")
-        self.assertEqual(new_row_names[-1], "copper-mg", "The content of the fields doesn't match")
-
-
-        pass
+    # def test_greek_db(self):
+    #
+    #     # old_row_names = greek_example_sheet.row[1]
+    #
+    #     process_greek_db(self.fields_correspondences)
+    #     greek_db_to_test = pe.get_sheet(file_name="data/processed-greek-db.xlsx")
+    #     new_row_names = greek_db_to_test.row[0]
+    #
+    #     # Check first and last name fields
+    #     self.assertEqual(new_row_names[0], "food-name", "The content of the fields doesn't match")
+    #     self.assertEqual(new_row_names[-1], "copper-mg", "The content of the fields doesn't match")
+    #
+    #
+    #     pass
 
     def test_italian_db(self):
 
