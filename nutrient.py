b0e4a8ce74c7b482b0db143983bc4e5554b51984
jimcase <jcaso@hotmail.es>
2019-05-16 20:48:15 +0200

    #1 add nutrient class and test

---
+++ nutrient.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+from foodlib import *
+
+
+# nutrient component
+class Nutrient:
+    # Las variables de clase son para atributos y métodos compartidos por todas las instancias de la clase  
+    
+    #CATEGORIES = list(Foodlib.CATEGORY_VALUES.keys()) 
+
+    # 'nutrient_category': kcal|kJ. https://www.boe.es/doue/2011/304/L00018-00063.pdf pág.45
+    ENERGY_VALUES = { "carbohydrate": (4,17), "polyalcohol": (2.4,10), "protein": (4,17), "fat": (9,37), "salatrim": (6,25),
+                "alcohol": (7,29), "etanol": (7,29), "organic-acid": (3,13), "dietary-fiber": (2,8), "eritritol": (0,0),
+                "mineral": (0,0), "vitamin": (0,0), "water": (0,0)
+
+    }
+
+    def __init__(self,scientific_param,names_param,unit_param,value_param,range_param1,range_param2,category_param):
+        # Variables de instancia: son para datos únicos de cada instancia
+
+        # - cientific name in english language.
+        self.scientific_name = scientific_param
+        # - nutrient component name in one or more different languages
+        self.multi_names = names_param
+        # - measure unit
+        self.unit = unit_param
+        # - the value
+        self.value = value_param
+        # - control range for the nutrient component value
+        self.control_range = (range_param1,range_param2)
+        # - nutrient component by a specific category fiel, (animal,vegetal,liquid,gas,other)
+        self.category = category_param
+        
+
+    
+    # Check if the current nutrient is in the control range
+    def InControlRange(self):
+
+        return self.value >= self.control_range[0] and self.value <= self.control_range[1]
+
+
+    
---
+++ test/test_nutrient.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+
+from nutrient import *
+
+class nutrientTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_InControlRange(self):
+        nutrient = Nutrient("potassium",["potasio","pottasche"],"g",7,3.5,5.3,"mineral")
+        self.assertEqual(nutrient.InControlRange(), False, "Incorrect values")
+        nutrient = Nutrient("potassium",["potasio","pottasche"],"g",5,3.5,5.3,"mineral")
+        self.assertEqual(nutrient.InControlRange(), True, "Incorrect values")
+
+
+    
+
+#test_nutrient = nutrientTestCase()
+#test_nutrient.test_InControlRange()
