460a975837be3f3c9dd8ee556fbcf6754da27604
jimcase <jcaso@hotmail.es>
2019-05-30 20:28:42 +0200

    #1 add food test

--- food.py
+++ food.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from enum import Enum
 
 from nutrient import *
+from food import *
 from foodlib import *
 
-# food component
-class Food:
-    # Las variables de clase son para atributos y métodos compartidos por todas las instancias de la clase  
-       
-    def __init__(self,names_param,alias_param,nutrients_param,description_param,action_param,unit_param,proteins_param,carbo_param,fat_param,pc_param):
-        # Variables de instancia: para datos únicos de cada instancia
 
+# food component
+class Food():
+    # Las variables de clase son para atributos y métodos compartidos por todas las instancias de la clase  
+    #f = foodlib.Foodlib()
+    #foodlib = Foodlib()
+
+    def __init__(self,names_param,alias_param,nutrients_param,description_param,action_param,unit_param,value_param,proteins_param,carbo_param,fat_param,pc_param):
+        # Variables de instancia: para datos únicos de cada instancia
+        
         # - food name in one or more different languages
         self.multi_names = names_param
-        # - food name in one or more different languages
+        # - food alias in one or more different languages
         self.multi_alias = alias_param
         # - one or more nutrient components. Considering all food has some nutritional value
         self.nutrients_list = nutrients_param        
@@ -22,8 +25,10 @@ class Food:
         self.description = description_param
         # - food action
         self.action = action_param
-        # - unit measure
+        # - measure unit 
         self.unit = unit_param
+        # - value
+        self.value = value_param        
         # - % total Protein
         self.protein_percent = proteins_param
         # - % total Carbohydrates
@@ -33,18 +38,65 @@ class Food:
         # - % total PC
         self.total_pc = pc_param       
         # - sum of total nutrient components weight value
-        self.total_weight = 0        
+        self.total_nutrients_weight = 0        
         self.total_kcal = 0
         # traverse each nutrient to sum the weights
-        for nutrient in nutrients_list:
-            if nutrient.value > 0:
-                self.total_weight += nutrient.value
-            self.total_kcal += (Foodlib.GetEnergyValue(nutrient))[0]
+        #for nutrient in self.nutrients_list:
+            #if nutrient.value > 0:
+                #self.total_nutrients_weight += nutrient.value
+                #self.total_kcal += (self.foodlib.GetEnergyValue(nutrient))[0]
         
-        
+        # - other components
+        self.other_components_value = self.value - self.total_nutrients_weight
 
-        
-        
+    # Method to check if two foods are equal
+    def EqualTo(self,food):            
+        equal = False
+        if isinstance(food,Food) and len(self.nutrients_list) == len(food.nutrients_list):
+            if set(self.nutrients_list) == set(food.nutrients_list):
+                equal = True
+        return  equal
 
-        
+    
+    # Method to check if the current food contains a specific nutrient
+    def HasNutrient(self,nutrient):
 
+        result = False        
+       
+        # Check if the parameter is a instance of Nutrient class
+        if isinstance(nutrient,Nutrient):
+
+            # Traverse each nutrient in the current food
+            for ntr in self.nutrients_list: 
+
+                # First, check the scientific_name property  
+                if nutrient.scientific_name.upper() ==  ntr.scientific_name.upper():
+                    # This condition satisfied is enough to assure equality
+                    result = True                    
+                    
+                # Traverse each parameter name
+                for nutrient_name in nutrient.multi_names:   
+                    # Check if the current parameter name is in some nutrient of nutrients_list
+                    if nutrient_name.upper() in [x.upper() for x in ntr.multi_names]:
+                        result = True
+
+                if result:
+                    break
+        
+                
+        return result
+
+    # Get the total weight of a specific nutrient name
+    def GetNutrientQuantity(self,nutrient_name):
+
+        result = 0
+        if nutrient_name and isinstance(nutrient_name, str):
+            # Traverse nutrient list
+            for ntr in self.nutrients_list:
+                # without distinction between uppercase and lowercase
+                if (nutrient_name.upper() == ntr.scientific_name.upper()) or (nutrient_name.upper() in [x.upper() for x in ntr.multi_names]):
+                    result = ntr.value
+                    # the search ends
+                    break
+        
+        return result
---
+++ test/test_food.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+import sys
+
+from nutrient import Nutrient
+from food import Food
+
+class foodTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_Equal(self):
+
+        #scientific_param,names_param,unit_param,value_param,range_param1,range_param2,category_param
+        nutrient1 = Nutrient("potassium",["potasio","pottasche"],"mg",126,125,127,"mineral")
+        nutrient2 = Nutrient("magnesium",["magnesio","magnésium"],"mg",10,9,11,"mineral")
+        nutrient3 = Nutrient("cholesterol",[],"g",373,370,375,"carbohydrate")
+
+        nutrient2 = Nutrient("sucrose",["sacarosa","saccharose"],"g",6,3.5,5.3,"carbohydrate")
+        
+        nutrient4 = Nutrient("lactose",["lactosa"],"g",6,3.5,5.3,"mineral")
+        nutrient5 = Nutrient("vitamin-b1-tiamina",["vitamina-A-IU"],"mg",6,3.5,5.3,"vitamin")
+        
+        
+        desctiption = "They are protected by a shell and rich in proteins"
+        action = ""
+        unit = "g"
+        value = 100
+        #names_param,alias_param,nutrients_param,description_param,action_param,unit_param,proteins_param,carbo_param,fat_param,pc_param
+        food_object1 = Food(["chicken-egg","huevo-de-gallina","Oeuf-de-poule"],[],[nutrient1,nutrient2,nutrient3],desctiption,action,unit,value,0,0,0,0)
+        food_object2 = Food(["chicken-egg","ovo-de-pollo","Oeuf-de-poule"],[],[nutrient1,nutrient2,nutrient3],desctiption,action,unit,value,0,0,0,0)
+        
+        self.assertEqual(food_object1.EqualTo(food_object2), True, "Incorrect value")
+        food_object1 = Food(["chicken-egg","huevo-de-gallina","Oeuf-de-poule"],[],[nutrient1,nutrient5,nutrient3],desctiption,action,unit,value,0,0,0,0)
+        self.assertEqual(food_object1.EqualTo(food_object2), False, "Incorrect value")
+
+    def test_HasNutrient(self):
+        nutrient1 = Nutrient("potassium",["potasio","pottasche"],"mg",126,125,127,"mineral")
+        nutrient2 = Nutrient("magnesium",["magnesio","magnésium"],"mg",10,9,11,"mineral")
+        nutrient3 = Nutrient("cholesterol",[],"g",373,370,375,"carbohydrate")                     
+        desctiption = "They are protected by a shell and rich in proteins"
+        action = ""
+        unit = "g"
+        value = 100
+        #names_param,alias_param,nutrients_param,description_param,action_param,unit_param,proteins_param,carbo_param,fat_param,pc_param
+        food_object1 = Food(["chicken-egg","huevo-de-gallina","Oeuf-de-poule"],[],[nutrient1,nutrient2,nutrient3],desctiption,action,unit,value,0,0,0,0)
+        self.assertEqual(food_object1.HasNutrient(nutrient2), True, "Incorrect nutrient")
+        
+        # Removing the scientific name and alias
+        nutrient2 = Nutrient("",["magnesio",""],"mg",10,9,11,"mineral")
+        self.assertEqual(food_object1.HasNutrient(nutrient2), True, "Incorrect nutrient")
+
+        nutrient2 = Nutrient("",["majnesio",""],"mg",10,9,11,"mineral")
+        self.assertEqual(food_object1.HasNutrient(nutrient2), False, "Incorrect nutrient")
+
+        nutrient2 = Nutrient("",["",""],"mg",10,9,11,"mineral")
+        self.assertEqual(food_object1.HasNutrient(nutrient2), False, "Incorrect nutrient")
+
+        self.assertEqual(food_object1.HasNutrient("nutrient2"), False, "Incorrect nutrient")
+
+
+    def test_GetNutrientQuantity(self):
+
+        nutrient1 = Nutrient("potassium",["potasio","pottasche"],"mg",126,125,127,"mineral")
+        nutrient2 = Nutrient("magnesium",["magnesio","magnésium"],"mg",10,9,11,"mineral")
+        nutrient3 = Nutrient("cholesterol",[],"g",373,370,375,"carbohydrate")                     
+        desctiption = "They are protected by a shell and rich in proteins"
+        action = ""
+        unit = "g"
+        value = 100
+        #names_param,alias_param,nutrients_param,description_param,action_param,unit_param,proteins_param,carbo_param,fat_param,pc_param
+        food_object1 = Food(["chicken-egg","huevo-de-gallina","Oeuf-de-poule"],[],[nutrient1,nutrient2,nutrient3],desctiption,action,unit,value,0,0,0,0)
+        self.assertEqual(food_object1.GetNutrientQuantity("cholesterol"), 373, "Incorrect nutrient")
+        
+        self.assertEqual(food_object1.GetNutrientQuantity("cHolesterol"), 373, "Incorrect nutrient")
+        self.assertEqual(food_object1.GetNutrientQuantity("cHol esterol"), 0, "Incorrect nutrient")
+        self.assertEqual(food_object1.GetNutrientQuantity(""), 0, "Incorrect nutrient")
+        self.assertEqual(food_object1.GetNutrientQuantity(777), 0, "Incorrect nutrient")
+        self.assertEqual(food_object1.GetNutrientQuantity("magnésium"), 10, "Incorrect nutrient")
+        
+
+
+
+
+
+
+
+test = foodTestCase()
+test.test_Equal()
+test.test_HasNutrient()
+test.test_GetNutrientQuantity()
+        
